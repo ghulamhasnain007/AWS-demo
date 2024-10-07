44c0eef8ce7b240a1379c40ebcc2510be6cf78b1
ghulamhasnain007 <ghasnain668@gmail.com>
2024-10-07 20:46:27 +0500

    add root

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+require("dotenv").config()
 const express = require('express');
 const cors = require("cors");
 
 const { route: userRoute } = require('./routes.js');
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
 app.use('/users', userRoute);
+app.use('/', (req, res)=>{
+    res.send("Hello from root")
+});
 
 // Bind the app to 0.0.0.0 so it can accept requests from outside
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is up and running on PORT: ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is up and running on PORT: ${PORT}`);
 });
 
 module.exports = { app };
